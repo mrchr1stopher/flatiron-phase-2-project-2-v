4950f0765630f20e86b6307721ed7f1d46970664
Christopher Wiggins <cwiggins610@gmail.com>
2023-10-02 18:52:53 -0400

    hello showing, bloat deleted

---
+++ src/componets/Header.jsx
@@ -0,0 +1,24 @@
+import react from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>my app</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#some-id">Some Link Ttime</a>
+          </li>
+          <li>
+            <a href="#some-id">Some Link Ttime</a>
+          </li>
+          <li>
+            <a href="#some-id">Some Link Ttime</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
