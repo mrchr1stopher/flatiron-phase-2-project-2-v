9585288777d04a974ea367f7bf6824f9ef50e473
Christopher Wiggins <cwiggins610@gmail.com>
2023-10-02 20:05:27 -0400

    NavBar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import Header from "./componets/Header";
 import Main from "./componets/Main";
 import Footer from "./componets/Footer";
 import WordBank from "./componets/WordBank";
+import NavBar from "./componets/NavBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Main />
-      <Footer />
+      <NavBar />
+      <Switch>
+        <Route exact path="/wordbank">
+          <WordBank />
+        </Route>
+        <Route exact path="/header">
+          <Header />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/componets/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "20px",
+  margin: "0 5px 5px",
+  background: "black",
+  color: "white",
+};
+
+function NavBar() {
+  return (
+    <div>
+      <NavLink
+        to="/header"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "black",
+          color: "red",
+        }}
+      >
+        head
+      </NavLink>
+
+      <NavLink
+        to="/wordbank"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "black",
+          color: "red",
+        }}
+      >
+        Wordlist
+      </NavLink>
+    </div>
+  );
+}
+
+export default NavBar;
