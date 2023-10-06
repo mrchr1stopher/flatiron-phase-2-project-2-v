d6034edef698780121df71c689ac02c0bf7bb392
Christopher Wiggins <cwiggins610@gmail.com>
2023-10-06 12:46:57 -0400

    points added to quiz

--- src/componets/Quiz.jsx
+++ src/componets/Quiz.jsx
@@ -4,36 +4,48 @@ const Quiz = () => {
   const [word, setWord] = useState();
   const [userAnswer, setUserAnswer] = useState("");
   const [showAnswer, setShowAnswer] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const fetchRandomWord = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/words");
+      if (!response.ok) {
+        throw Error("Error Loading Random Words");
+      }
+      const data = await response.json();
+
+      const randomList = Math.floor(Math.random() * data.length);
+      const randomWord = data[randomList];
+      setWord(randomWord);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchRandomWord = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/words");
-        if (!response.ok) {
-          throw Error("Error Loading Random Words");
-        }
-        const data = await response.json();
-
-        const randomList = Math.floor(Math.random() * data.length);
-        const randomWord = data[randomList];
-        setWord(randomWord);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
     fetchRandomWord();
   }, []);
 
   const checkAnswer = () => {
     setShowAnswer(true);
+    if (userAnswer.toLowerCase() === word?.spanish.toLowerCase()) {
+      setScore((prevScore) => prevScore + 1);
+    }
+  };
+
+  const getNextWord = () => {
+    setUserAnswer("");
+    setShowAnswer(false);
+    fetchRandomWord();
   };
 
   return (
     <div>
       <h1>English to Spanish Quiz</h1>
+
       <div>
         <p>Translate the English word to Spanish:</p>
+        <p>Score: {score}</p>
         {word && (
           <div className="image-container">
             <img
@@ -44,6 +56,7 @@ const Quiz = () => {
           </div>
         )}
         {word && <p>{word.english}</p>}
+
         <input
           type="text"
           placeholder="Your answer"
@@ -52,6 +65,11 @@ const Quiz = () => {
         />
         <button onClick={checkAnswer}>Check Answer</button>
         {showAnswer && <p>Correct Answer: {word.spanish}</p>}
+        {showAnswer && (
+          <div>
+            <button onClick={getNextWord}>Next Word</button>
+          </div>
+        )}
       </div>
     </div>
   );
