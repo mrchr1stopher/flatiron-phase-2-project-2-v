c8af1bf043fc5e6f1945530e10c2271ae67ca5a8
Christopher Wiggins <cwiggins610@gmail.com>
2023-10-07 12:06:23 -0400

    add css to combo counter

--- src/componets/ComboCounter.jsx
+++ src/componets/ComboCounter.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import "./ComboCounter.css";
 
 const ComboCounter = ({ comboCount }) => {
   let comboWord = "";
+  let comboClass = "";
 
   if (comboCount >= 10) {
     comboWord = "PERFECTO";
+    comboClass = "gold";
   } else if (comboCount >= 7) {
     comboWord = "EXCELENTE";
+    comboClass = "silver";
   } else if (comboCount >= 4) {
     comboWord = "MUY BIEN";
+    comboClass = "bronze";
   } else if (comboCount >= 2) {
     comboWord = "BIEN";
+    comboClass = "bronze";
   }
 
-  return <div>{comboWord}</div>;
+  return <div className={`combocountertext ${comboClass}`}>{comboWord}</div>;
 };
 
 export default ComboCounter;
